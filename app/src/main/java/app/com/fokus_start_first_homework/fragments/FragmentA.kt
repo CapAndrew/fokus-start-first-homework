6959864cd0fdca0289b4c29460cb22c0f2d5dac2
glavatskiy <a.glavatskiy@cft.ru>
2020-10-25 13:03:58 +0700

    Первое домашнее задание

--- app/src/main/java/app/com/fokus_start_first_homework/fragments/FragmentA.kt
+++ app/src/main/java/app/com/fokus_start_first_homework/fragments/FragmentA.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import app.com.fokus_start_first_homework.R
 import kotlinx.android.synthetic.main.fragment_a.*
 
@@ -26,12 +26,20 @@ class FragmentA : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        if (parentFragmentManager.backStackEntryCount > 0) {
+            parentFragmentManager.popBackStack(
+                parentFragmentManager.getBackStackEntryAt(0).id,
+                FragmentManager.POP_BACK_STACK_INCLUSIVE
+            )
+        }
+
         return inflater.inflate(R.layout.fragment_a, container, false)
     }
 
     private fun openFragmentB() {
-       parentFragmentManager.beginTransaction()
+        parentFragmentManager.beginTransaction()
             .replace(R.id.container, FragmentB.newInstance())
+            .addToBackStack(null)
             .commit()
     }
 }
